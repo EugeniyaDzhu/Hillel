b0ecaf7fbb599a8068ec6f68a60d83dee666abee
PimaUA <pimaua87@gmail.com>
2022-07-28 18:55:21 +0300

    restore class

--- src/com/hillel/IvanovDmitro/Hello.java
+++
@@ -1,8 +0,0 @@
-package com.hillel.IvanovDmitro;
-
-public class Hello {
-
-    public static void main(String[] args) {
-        System.out.println("Hello Java");
-    }
-}
--- src/com/pimaua/homework/Homework1.java
+++
@@ -1,7 +0,0 @@
-package com.pimaua.homework;
-
-public class Homework1 {
-    public static void main(String[] args) {
-        System.out.println("Hello Java Pro!!!");
-    }
-}
