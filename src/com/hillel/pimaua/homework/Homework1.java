10eaefb691572798f2eb32c32fb143f8440f136d
PimaUA <pimaua87@gmail.com>
2022-07-28 19:03:52 +0300

    restore class again

--- src/com/hillel/pimaua/homework/Homework1.java
+++ src/com/hillel/pimaua/homework/Homework1.java
@@ -2,6 +2,6 @@ package com.hillel.pimaua.homework;
 
 public class Homework1 {
     public static void main(String[] args) {
-        System.out.println("Hello Java!!!");
+        System.out.println("Hello Java Pro!");
 }
 }
