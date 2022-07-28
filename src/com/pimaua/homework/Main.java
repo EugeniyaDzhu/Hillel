a0aec5ee033ecedf5e729229cfed47185b2b2692
PimaUA <pimaua87@gmail.com>
2022-07-28 18:18:43 +0300

    restore

--- src/com/pimaua/homework/Main.java
+++ src/com/pimaua/homework/Main.java
@@ -2,8 +2,6 @@ package com.pimaua.homework;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Java");
-        System.out.println("Hello EugeniyaDzhu");
-        System.out.println("Hello Java Pro");
+        System.out.println("Hello Java Pro!!!");
     }
 }
