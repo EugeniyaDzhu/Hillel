45b4e31479a89cfdb3d1d30784ce6d04a4cda95b
EugeniyaDzhu <eugeniya.dzhu@gmail.com>
2022-07-29 20:10:00 +0300

    2

---
+++ src/com/hillel/GitAxomple.java
@@ -0,0 +1,12 @@
+package com.hillel;
+
+public class GitAxomple {
+    public void method() {
+        System.out.println(1);
+        System.out.println(22);
+        System.out.println(333);
+        System.out.println(1);
+        System.out.println(3);
+        System.out.println(5);
+    }
+}
